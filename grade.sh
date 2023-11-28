241874cc8e0e71697d75352099c0a6c69ea583dc
amsapatnekar <146883547+amsapatnekar@users.noreply.github.com>
2023-11-28 12:37:13 -0800

    Created autograder

--- grade.sh
+++ grade.sh
@@ -9,6 +9,24 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 
+files=`find . -name "*.java" `
+for file in $files
+do 
+    if [[ -f $file ]] && [[ $file == **/ListExamples.java* ]]
+    then 
+        echo 'File found' 
+        cp $file grading-area/
+        cp -r lib grading-area/
+    fi 
+done    
+cd grading-area
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test.txt
+
+grep "test" test.txt
+grep -Eo '[0-9]{1,4}' test.txt
+
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
